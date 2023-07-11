f8a874d2c386f5f9dd20ed0ad35e4aec8dd8b52a
unknown <ethowtv@gmail.com>
2023-07-11 04:10:05 +0300

    server async

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,18 @@ const apiRoutes = require('./routes/index')
 const cookieParser = require('cookie-parser');
 const PORT = 4000;
 
-connectDB();
+
+const connectToDB = async () => {
+    try {
+        const connect = await connectDB("mongodb+srv://fitsacreation:LhaWy3tQpcf9p0TN@cluster0.4e3bpnj.mongodb.net/ecommerce");
+        console.log("MongoDB connected: " + connect.connection.host)
+        app.listen(PORT, () => { console.log("Server started") })
+    } catch (error) {
+        console.log(error?.message)
+    }
+}
+connectToDB();
+
 const app = express();
 app.use(cookieParser())
 
@@ -23,5 +34,3 @@ app.use(express.json())
 app.use('/', seeder)
 app.use('/api/v1', authRoute)
 app.use('/api/v1', apiRoutes)
-
-app.listen(PORT, () => { console.log("Server started") }) 
--- backend/model/db.js
+++ backend/model/db.js
@@ -1,17 +1,10 @@
 const mongoose = require('mongoose')
 
-const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect("mongodb+srv://fitsacreation:LhaWy3tQpcf9p0TN@cluster0.4e3bpnj.mongodb.net/ecommerce", {
-            useUnifiedTopology: true,
-            useNewUrlParser: true,
-        });
-
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.error(`Error: ${error.message}`);
-        process.exit(1);
-    }
+const connectDB = (url) => {
+    return mongoose.connect(url, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+    });
 };
 
 module.exports = connectDB;
